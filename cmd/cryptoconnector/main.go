e839a74d6151d03f7827c60f18b583f58b18f568
Lyche <caroxmeme@gmail.com>
2025-10-20 18:30:08 +0700

    Add main.go
    Co-authored-by: Hajigur <66867581+hajigur69@users.noreply.github.com>

---
+++ cmd/cryptoconnector/main.go
@@ -0,0 +1,23 @@
+// cmd/cryptoconnector/main.go
+package main
+
+import (
+    "flag"
+    "log"
+    "os"
+
+    "cryptoconnector/internal/cryptoconnector"
+)
+
+func main() {
+    verbose := flag.Bool("verbose", false, "Enable verbose logging")
+    input := flag.String("input", "", "Input file path")
+    output := flag.String("output", "", "Output file path")
+    flag.Parse()
+
+    app := cryptoconnector.NewApp(*verbose)
+    if err := app.Run(*input, *output); err != nil {
+        log.Printf("Error: %v", err)
+        os.Exit(1)
+    }
+}
